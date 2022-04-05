74bd3edfe71a4365880bbf900b0dc9778956ad76
JeremyM45 <jeremyvs1945@gmail.com>
2022-04-05 16:25:39 -0700

    add image and description for movies

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
-function Movie(name, times, priceMod)  {
+function Movie(name, times, priceMod, description, img)  {
   this.name = name
   this.times = times
   this.priceMod = priceMod
+  this.description = description
+  this.img = img
 }
 function MovieList()  {
   this.movies = {}
@@ -47,10 +49,10 @@ function addTimesToList(movie)  {
 
 
 let movieList = new MovieList();
-const movie1 = new Movie("SONIC THE HEDGEHOG 2", ["7:30pm","8:30pm","9:30pm", "10:30pm"], 1)
-const movie2 = new Movie("SPIDER-MAN: NO WAY HOME", ["5:30pm", "6:30pm", "7:30pm"], 0.9)
-const movie3 = new Movie("UNCHARTED", ["9:00am", "11:00am"], 0.85)
-const movie4 = new Movie("TOP GUN: MAVERICK", ["8:00pm", "9:00pm", "10:45pm"], 1)
+const movie1 = new Movie("SONIC THE HEDGEHOG 2", ["7:30pm","8:30pm","9:30pm", "10:30pm"], 1, "When the manic Dr Robotnik returns to Earth with a new ally, Knuckles the Echidna, Sonic and his new friend Tails is all that stands in their way.", "<img src='img/Sonic.jpg' alt='A picture of a Sonic 2 Poster'>")
+const movie2 = new Movie("SPIDER-MAN: NO WAY HOME", ["5:30pm", "6:30pm", "7:30pm"], 0.9, "With Spider-Man's identity now revealed, Peter asks Doctor Strange for help. When a spell goes wrong, dangerous foes from other worlds start to appear, forcing Peter to discover what it truly means to be Spider-Man.", "<img src='img/Spider-Man.jpg' alt='A picture of a Spider-Man No Way Home Poster'>")
+const movie3 = new Movie("UNCHARTED", ["9:00am", "11:00am"], 0.85, "Street-smart Nathan Drake is recruited by seasoned treasure hunter Victor 'Sully' Sullivan to recover a fortune amassed by Ferdinand Magellan, and lost 500 years ago by the House of Moncada.", "<img src='img/Uncharted.jpg' alt='A picture of a Uncharted Poster'>")
+const movie4 = new Movie("TOP GUN: MAVERICK", ["8:00pm", "9:00pm", "10:45pm"], 1, "After more than thirty years of service as one of the Navy's top aviators, Pete Mitchell is where he belongs, pushing the envelope as a courageous test pilot and dodging the advancement in rank that would ground him.", "<img src='img/Top-Gun.jpg' alt='A picture of a Top Gun Maverick Poster'>")
 movieList.addMovie(movie1)
 movieList.addMovie(movie2)
 movieList.addMovie(movie3)
@@ -70,7 +72,9 @@ $(document).ready(function()  {
     let ticket = new Ticket(movie, time)
     let price = ticket.priceFormula(movieList.movieGetter(movie).priceMod, age);
     $("#title").text(ticket.movieName)
-    $("#time").text(ticket.time)
-    $("#cost").text("$" + price)
+    $("#picture").html(movieList.movieGetter(movie).img)
+    $("#description").text(movieList.movieGetter(movie).description)
+    $("#time").text("Time: " + ticket.time)
+    $("#cost").text("Price: $" + price)
   });
 });
